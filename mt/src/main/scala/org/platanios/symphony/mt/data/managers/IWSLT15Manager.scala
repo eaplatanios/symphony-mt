5a210f6a7763996e1912d3ce56c24505153f0ac7
Anthony Platanios <e.a.platanios@gmail.com>
2018-01-23 16:58:21 -0500

    Minor bug fix for the IWSLT15 data manager.

--- mt/src/main/scala/org/platanios/symphony/mt/data/managers/IWSLT15Manager.scala
+++ mt/src/main/scala/org/platanios/symphony/mt/data/managers/IWSLT15Manager.scala
@@ -101,7 +101,7 @@ object IWSLT15Manager {
   val testPrefix : String = "tst2013"
   val vocabPrefix: String = "vocab"
 
-  val supportedLanguagePairs: Set[(Language, Language)] = Set((Vietnamese, English))
+  val supportedLanguagePairs: Set[(Language, Language)] = Set((English, Vietnamese))
 
   def isLanguagePairSupported(srcLanguage: Language, tgtLanguage: Language): Boolean = {
     supportedLanguagePairs.contains((srcLanguage, tgtLanguage)) ||
