640571fb933f26e30c5f8a873ec0871a56517faf
Anthony Platanios <e.a.platanios@gmail.com>
2018-04-27 17:12:15 -0400

    [MODELS] Reverted the tensor array changes because they introduced various issues.

--- mt/src/main/scala/org/platanios/symphony/mt/models/parameters/WordEmbeddingsPerLanguage.scala
+++ mt/src/main/scala/org/platanios/symphony/mt/models/parameters/WordEmbeddingsPerLanguage.scala
@@ -25,9 +25,11 @@ import scala.collection.mutable
   * @author Emmanouil Antonios Platanios
   */
 class WordEmbeddingsPerLanguage protected (
-    override val embeddingsSize: Int
+    override val embeddingsSize: Int,
+    val mergedEmbeddings: Boolean,
+    val mergedProjections: Boolean
 ) extends WordEmbeddingsType {
-  override type T = tf.TensorArray
+  override type T = Seq[Output]
 
   override def createStringToIndexLookupTable(languages: Seq[(Language, Vocabulary)]): Output = {
     val tables = languages.map(l => l._2.stringToIndexLookupTable(name = l._1.name))
@@ -39,18 +41,18 @@ class WordEmbeddingsPerLanguage protected (
     tf.stack(tables.map(_.handle))
   }
 
-  override def createWordEmbeddings(languages: Seq[(Language, Vocabulary)]): tf.TensorArray = {
+  override def createWordEmbeddings(languages: Seq[(Language, Vocabulary)]): Seq[Output] = {
     val embeddingsInitializer = tf.RandomUniformInitializer(-0.1f, 0.1f)
-    val tensorArray = tf.TensorArray.create(
-      size = languages.size,
-      dataType = FLOAT32,
-      dynamicSize = false,
-      clearAfterRead = false,
-      elementShape = Shape(-1, embeddingsSize))
-    languages.zipWithIndex.foldLeft(tensorArray) {
-      case (ta, (language, index)) =>
-        ta.write(index, tf.variable(
-          language._1.name, FLOAT32, Shape(language._2.size, embeddingsSize), embeddingsInitializer).value)
+    if (!mergedEmbeddings) {
+      languages.map(l =>
+        tf.variable(l._1.name, FLOAT32, Shape(l._2.size, embeddingsSize), embeddingsInitializer).value)
+    } else {
+      val vocabSizes = languages.map(_._2.size)
+      val merged = tf.variable(
+        "Embeddings", FLOAT32, Shape(vocabSizes.sum, embeddingsSize), embeddingsInitializer).value
+      val sizes = tf.createWithNameScope("VocabularySizes")(tf.stack(vocabSizes.map(tf.constant(_))))
+      val offsets = tf.concatenate(Seq(tf.zeros(sizes.dataType, Shape(1)), tf.cumsum(sizes)(0 :: -1)))
+      Seq(merged, offsets)
     }
   }
 
@@ -59,45 +61,72 @@ class WordEmbeddingsPerLanguage protected (
   }
 
   override def embeddingLookup(
-      embeddingTables: tf.TensorArray,
+      embeddingTables: Seq[Output],
       languageIds: Seq[Output],
       languageId: Output,
       keys: Output,
       context: Option[(Output, Output)]
   ): Output = {
-    embeddingTables.read(languageId).gather(keys)
+    if (!mergedEmbeddings) {
+      val predicates = embeddingTables.zip(languageIds).map {
+        case (embeddings, langId) => (tf.equal(languageId, langId), () => embeddings)
+      }
+      val default = () => embeddingTables.head
+      tf.cases(predicates, default).gather(keys)
+    } else {
+      val merged = embeddingTables(0)
+      val offsets = embeddingTables(1)
+      merged.gather(keys + offsets.gather(languageId))
+    }
   }
 
   override def projectionToWords(
       languages: Seq[(Language, Vocabulary)],
       languageIds: Seq[Output],
-      projectionsToWords: mutable.Map[Int, tf.TensorArray],
+      projectionsToWords: mutable.Map[Int, Seq[Output]],
       inputSize: Int,
       languageId: Output
   ): Output = {
-    projectionsToWords
-        .getOrElseUpdate(inputSize, {
-          val weightsInitializer = tf.RandomUniformInitializer(-0.1f, 0.1f)
-          val tensorArray = tf.TensorArray.create(
-            size = languages.size,
-            dataType = FLOAT32,
-            dynamicSize = false,
-            clearAfterRead = false,
-            elementShape = Shape(inputSize, -1))
-          languages.zipWithIndex.foldLeft(tensorArray) {
-            case (ta, (language, index)) =>
-              ta.write(index, tf.variable(
-                s"${language._1.name}/OutWeights", FLOAT32, Shape(inputSize, language._2.size),
-                weightsInitializer).value)
-          }
-        }).read(languageId)
+    if (!mergedProjections) {
+      val projectionsForSize = projectionsToWords
+          .getOrElseUpdate(inputSize, {
+            val weightsInitializer = tf.RandomUniformInitializer(-0.1f, 0.1f)
+            languages.map(l => tf.variable(
+              s"${l._1.name}/OutWeights", FLOAT32, Shape(inputSize, l._2.size), weightsInitializer).value)
+          })
+      val predicates = projectionsForSize.zip(languageIds).map {
+        case (projections, langId) => (tf.equal(languageId, langId), () => projections)
+      }
+      val default = () => projectionsForSize.head
+      tf.cases(predicates, default)
+    } else {
+      val projectionsForSize = projectionsToWords
+          .getOrElseUpdate(inputSize, {
+            val weightsInitializer = tf.RandomUniformInitializer(-0.1f, 0.1f)
+            val vocabSizes = languages.map(_._2.size)
+            val merged = tf.variable(
+              "ProjectionWeights", FLOAT32, Shape(inputSize, vocabSizes.sum), weightsInitializer).value
+            val sizes = tf.createWithNameScope("VocabularySizes")(tf.stack(vocabSizes.map(tf.constant(_))))
+            val offsets = tf.concatenate(Seq(tf.zeros(sizes.dataType, Shape(1)), tf.cumsum(sizes)(0 :: -1)))
+            Seq(merged, offsets, sizes)
+          })
+      val merged = projectionsForSize(0)
+      val offsets = projectionsForSize(1)
+      val sizes = projectionsForSize(2)
+      tf.slice(
+        merged,
+        tf.stack(Seq(0, offsets.gather(languageId))),
+        tf.stack(Seq(inputSize, sizes.gather(languageId))))
+    }
   }
 }
 
 object WordEmbeddingsPerLanguage {
   def apply(
-      embeddingsSize: Int
+      embeddingsSize: Int,
+      mergedEmbeddings: Boolean = false,
+      mergedProjections: Boolean = false
   ): WordEmbeddingsPerLanguage = {
-    new WordEmbeddingsPerLanguage(embeddingsSize)
+    new WordEmbeddingsPerLanguage(embeddingsSize, mergedEmbeddings, mergedProjections)
   }
 }
