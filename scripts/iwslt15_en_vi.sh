8402a99bc09b8c6941bbcbbeb8494b52bd983841
Anthony Platanios <e.a.platanios@gmail.com>
2018-10-22 00:42:18 -0400

    [EXPERIMENTS] Minor edit.

--- scripts/iwslt15_en_vi.sh
+++ scripts/iwslt15_en_vi.sh
@@ -7,7 +7,7 @@ java -jar target/scala-2.12/symphony-mt-0.1.0-SNAPSHOT.jar \
   --data-dir temp/data \
   --dataset iwslt15 \
   --languages en:vi \
-  --eval-languages en:cs,en:de,en:fr,en:th,en:vi,en:zh \
+  --eval-languages en:vi \
   --use-back-translations \
   --parallel-portion 1.00 \
   --eval-datasets dev2010:1.00,tst2013:1.00 \
