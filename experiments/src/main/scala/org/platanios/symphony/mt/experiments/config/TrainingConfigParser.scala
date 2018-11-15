839f1065ed35fc62009ff7e96fe6480d1615baa4
Anthony Platanios <e.a.platanios@gmail.com>
2018-11-14 19:53:12 -0500

    [EXPERIMENTS] Added support for some more optimizer configuration parameters.

--- experiments/src/main/scala/org/platanios/symphony/mt/experiments/config/TrainingConfigParser.scala
+++ experiments/src/main/scala/org/platanios/symphony/mt/experiments/config/TrainingConfigParser.scala
@@ -62,10 +62,22 @@ class TrainingConfigParser(
           case "adafactor" => tf.train.Adafactor(learningRate, learningRateSummaryTag = "LearningRate")
           case "adagrad" => tf.train.AdaGrad(learningRate.get, learningRateSummaryTag = "LearningRate")
           case "rmsprop" => tf.train.RMSProp(learningRate.get, learningRateSummaryTag = "LearningRate")
-          case "adam" => tf.train.Adam(learningRate.get, learningRateSummaryTag = "LearningRate")
-          case "lazy_adam" => tf.train.LazyAdam(learningRate.get, learningRateSummaryTag = "LearningRate")
-          case "amsgrad" => tf.train.AMSGrad(learningRate.get, learningRateSummaryTag = "LearningRate")
-          case "lazy_amsgrad" => tf.train.LazyAMSGrad(learningRate.get, learningRateSummaryTag = "LearningRate")
+          case "adam" =>
+            val beta1 = config.get[Float]("optimization.beta1", 0.9f)
+            val beta2 = config.get[Float]("optimization.beta2", 0.999f)
+            tf.train.Adam(learningRate.get, beta1 = beta1, beta2 = beta2, learningRateSummaryTag = "LearningRate")
+          case "lazy_adam" =>
+            val beta1 = config.get[Float]("optimization.beta1", 0.9f)
+            val beta2 = config.get[Float]("optimization.beta2", 0.999f)
+            tf.train.LazyAdam(learningRate.get, beta1 = beta1, beta2 = beta2, learningRateSummaryTag = "LearningRate")
+          case "amsgrad" =>
+            val beta1 = config.get[Float]("optimization.beta1", 0.9f)
+            val beta2 = config.get[Float]("optimization.beta2", 0.999f)
+            tf.train.AMSGrad(learningRate.get, beta1 = beta1, beta2 = beta2, learningRateSummaryTag = "LearningRate")
+          case "lazy_amsgrad" =>
+            val beta1 = config.get[Float]("optimization.beta1", 0.9f)
+            val beta2 = config.get[Float]("optimization.beta2", 0.999f)
+            tf.train.LazyAMSGrad(learningRate.get, beta1 = beta1, beta2 = beta2, learningRateSummaryTag = "LearningRate")
           case "yellowfin" => tf.train.YellowFin(learningRate.get, learningRateSummaryTag = "LearningRate")
           case _ => throw new IllegalArgumentException(s"'$optimizer' does not represent a valid optimizer.")
         },
@@ -144,10 +156,18 @@ class TrainingConfigParser(
     val stringBuilder = new StringBuilder()
     stringBuilder.append(s"bd:$bothDirections")
     stringBuilder.append(s".it:${bothDirections && config.get[Boolean]("use-identity-translations")}")
-    stringBuilder.append(s".ls:${config.get[Float]("label-smoothing")}")
-    stringBuilder.append(s".opt:${config.get[String]("optimization.optimizer")}")
+    stringBuilder.append(s".ls:${config.get[String]("label-smoothing")}")
+
+    val optimizer = config.get[String]("optimization.optimizer")
+    stringBuilder.append(s".opt:$optimizer")
     if (config.hasPath("optimization.learning-rate"))
-      stringBuilder.append(s"${config.get[Float]("optimization.learning-rate")}")
+      stringBuilder.append(s":${config.get[String]("optimization.learning-rate")}")
+    optimizer match {
+      case "adam" | "lazy_adam" | "amsgrad" | "lazy_amsgrad" =>
+        stringBuilder.append(s":beta1:${config.get[String]("optimization.beta1", "0.9")}")
+        stringBuilder.append(s":beta2:${config.get[String]("optimization.beta2", "0.999")}")
+      case _ => ()
+    }
 
     if (config.hasPath("curriculum.type")) {
       val curriculumType = config.get[String]("curriculum.type")
